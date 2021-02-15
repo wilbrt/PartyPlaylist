306eb0aae144b3a19d4bad8879375cecdb87145d
Vilppu Saarinen <vilppusaa@gmail.com>
2021-02-15 15:48:53 +0200

    everything

--- project.clj
+++ project.clj
@@ -6,7 +6,11 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [org.clojure/java.jdbc "0.7.10"]
+                 [org.postgresql/postgresql "42.2.9"]
+                 [hiccup "1.0.5"]
+                 [clj-http "3.12.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
