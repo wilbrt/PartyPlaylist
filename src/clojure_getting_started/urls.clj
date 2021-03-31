9541298bba2ee5448f8825ba407cddc8633f9002
Vilppu Saarinen <vilppusaa@gmail.com>
2021-03-31 18:40:22 +0300

    Added (shabby) spotify support

--- src/clojure_getting_started/urls.clj
+++ src/clojure_getting_started/urls.clj
@@ -16,45 +16,57 @@
       (jdbc/create-table-ddl r [[:id :int] [:url "varchar(255)"] [:name "varchar(255)"] [:source :int]]
                                   {:entities clojure.string/upper-case :conditional? true})
       #_(vec (str r))
-      (jdbc/db-do-commands spec r)
-      )
+      (jdbc/db-do-commands spec r))
   (html [:meta {:http-equiv "refresh" :content (str "0; URL=./videot?huone=" (str (first (get-in req [:params :nimi]))))}]
         [:p "Your room is being prepared."]))
 
-(defn videoid [query]
+(defn videoid [query source]
   (as-> query t
         (str t)
-        (client/get "https://www.youtube.com/results" {:query-params {"search_query" t}})
-        (str t)
-        (.split t "videoId")
+        (cond (= source 0) (as->  t a
+                                  (client/get "https://www.youtube.com/results" {:query-params {"search_query" a}})
+                                  (str a)
+                                  (.split a "videoId"))
+              (= source 1) (as-> t a
+                                 (client/get (str "http://elegant-croissant.glitch.me/spotify?type=track&q=" a) {:accept :json})
+                                 (str a)
+                                 (.split a "spotify:track:")))
         (second t )
         (.split t "\"")
-        (nth t 2)
+        (cond (= source 0) (->  t
+                                  (nth 2))
+              (= source 1) (->  t
+                                  (first)))
         (str t)
         (subs t 0 (- (.length t) 1))))
 
 
-(defn videoname [query]
+(defn videonametube [query]
   (let [teksti (str (client/get (str "https://www.youtube.com/watch?v=" query)))
         t (second (.split teksti "title>"))]
     (clojure.string/join "-" (reverse  (rest  (reverse (.split (subs t 0 (- (.length t) 2)) "-")))))))
 
-(defn get-url-by-id
-  [id table]
-  (let [q [(str "SELECT url FROM " table " WHERE id = ?") id]]
-      (->> q
-          (jdbc/query spec)
-          (first)
-          (:url))))
+(defn videonamespot [])
 
-(defn get-name-by-id
-  [id table]
-  (let [q [(str "SELECT name FROM " table " WHERE id = ?") id]]
-      (->> q
-          (jdbc/query spec)
-          (first)
-          (:name))))
+(defn videoname [query source]
+  (cond (= source 0) (videonametube (videoid query 0))
+        (= source 1) query))
 
+(defn get-url-by-id [id table]
+            (let [query [(str "SELECT url FROM " table " WHERE id = ?") id]
+                  result (jdbc/query spec query)]
+                          (:url (first result))))
+
+(defn get-source-by-id [id table]
+            (let [query [(str "SELECT source FROM " table " WHERE id = ?") id]
+                  result (jdbc/query spec query)]
+                          (:source (first result))))
+
+
+(defn get-name-by-id [id table]
+            (let [query [(str "SELECT name FROM " table " WHERE id = ?") id]
+                  result (jdbc/query spec query)]
+                          (:name (first result))))
 
 (defn newsongnumber [n table]
   (if-not (get-url-by-id n table)
@@ -64,105 +76,63 @@
 (defn create-url!
   "Given a url as a string, creates a url row in the database and returns
    the created row."
-  [url name table]
-  (let [id (newsongnumber 1 table)
-        row {:name name :url url :id id}
-        result (jdbc/insert! spec (keyword table) row)]
-    (first result)))
+  [url name table source]
+  (->> {:name name :url url :id (newsongnumber 1 table) :source source}
+       (jdbc/insert! spec (keyword table))
+       (first)))
 
 
 (defn create-url-handler [req]
   (let [r (str (first (get-in req [:params :url])))
-        table (get-in req [:params :huone])]
-        (as-> (videoid r) q
-              (create-url! q (videoname q) table))))
+        table (get-in req [:params :huone])
+        source (Integer/parseInt (get-in req [:params :source]))]
+    (str r table source)
+    (-> (videoid r source)
+              (create-url! (videoname r source) table source))))
 
 (defn update-table! [table]
       (jdbc/delete! spec (keyword table) ["id = ?" 1])
       (jdbc/execute! spec [(str "update " table " set id = id - 1 where id < ?") 30]))
 
-(defn player [table id]
-  (if id (html  [:script "$(function() {
-                          $('#addtolist').ajaxForm(function() {
-                              document.getElementById('lista').src = document.getElementById('lista').src;});});"]
-                [:iframe { :width "560" :height "300" :src (str "https://www.youtube.com/embed/" id "?autoplay=1")
-                :frameboarder "0" :allow "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"}])
+(defn player [table id source]
+  #_(html [:iframe { :width "300" :height "380" :src "https://open.spotify.com/embed/track/5XcZRgJv3zMhTqCyESjQrF"
+  :frameboarder "0" :allow "encrypted-media;" :allowtransparency "true"}])
+  (if (and id source) (cond
+                              (= source 0) (html  [:script "$(function() {
+                                                $('#addtolist').ajaxForm(function() {
+                                                  document.getElementById('lista').src = document.getElementById('lista').src;});});"]
+                                   [:iframe { :width "560" :height "300" :src (str "https://www.youtube.com/embed/" id "?autoplay=1")
+                                              :frameboarder "0" :allow "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"}])
+                (= source 1) (html [:script "$(function() {
+                                                $('#addtolist').ajaxForm(function() {
+                                                  document.getElementById('lista').src = document.getElementById('lista').src;});});"]
+                                   [:iframe { :width "300" :height "380" :src (str "https://open.spotify.com/embed/track/" id)
+                                              :frameboarder "0" :allow "encrypted-media;" :allowtransparency "true"}]))
          (html [:script "$(function() {
               $('#addtolist').ajaxForm(function() {
-                        location.reload();
-                    });
-                });"]
+                        location.reload();});});"]
                [:p "Playlist empty"])))
 
 (defn videohaku [req]
   (let [table (get-in req [:params :huone])
-        url (get-url-by-id 1 table)]
+        url (get-url-by-id 1 table)
+        sorsa (get-source-by-id 1 table)]
       (html [:h [:script {:src "https://code.jquery.com/jquery-3.5.1.min.js" :integrity "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" :crossorigin "anonymous"}]
                 [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" :integrity "sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" :crossorigin "anonymous"}]]
-            [:div {:align "center"} (player table url)]
+            [:div {:align "center"} (player table url sorsa)]
             [:div {:align "center"} [:p (get-name-by-id 1 table)]]
             [:div {:align "center"}
                 [:form {:id "addtolist" :action "./asd" :onclick "setTimeout(() => {document.getElementById('lista').src = document.getElementById('lista').src;}, 2000);" :method "post"}
                       [:input {:type "text" :id "url" :name "url"}]
+                      [:select {:id "source" :name "source" :form_id "addtolist"}
+                          [:option {:value 0} "Youtube"]
+                          [:option {:value 1} "Spotify"]]
                       [:input {:type "hidden" :id "huone" :name "huone" :value table}]
                       [:input {:type "submit" :id "url" :name "url"}]]
                 [:button {:type "submit" :value "Next" :onclick (str "window.location=\"./seuraava?huone=" table "\";")} "Next"]
                            #_[:button {:type "submit" :value "ref" :onclick "document.getElementById('lista').src = document.getElementById('lista').src"} "Refresh Playlist"]]
             [:div {:align "center"}
                 [:iframe {:id "lista" :width "450" :height "315" :src (str "./soittolista?huone=" table)}]])))
-
-
-
-(defn videohaku1 [req]
-    (let [table (get-in req [:params :huone])
-          id (get-url-by-id 1 table)]
-        (if id
-            (html [:h
-                   [:script
-                    {:src "https://code.jquery.com/jquery-3.5.1.min.js" :integrity "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" :crossorigin "anonymous"}]
-                   [:script
-                    {:src "https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" :integrity "sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" :crossorigin "anonymous"}]
-                   [:script "$(function() {
-                        $('#addtolist').ajaxForm(function() {
-                                  document.getElementById('lista').src = document.getElementById('lista').src;
-                              });
-                          });"]]
-                  [:div {:align "center"}
-                   [:iframe {
-                        :width "560"
-                        :height "300"
-                        :src (str "https://www.youtube.com/embed/" id "?autoplay=1")
-                        :frameboarder "0"
-                        :allow "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"}]]
-                  [:div {:align "center"}
-                   [:p (get-name-by-id 1 table)]]
-                  [:div {:align "center"}
-                        [:form {:id "addtolist" :action "./asd" :onclick "setTimeout(() => {document.getElementById('lista').src = document.getElementById('lista').src;}, 2000);" :method "post"}
-                        [:input {:type "text" :id "url" :name "url"}]
-                        [:input {:type "hidden" :id "huone" :name "huone" :value table}]
-                        [:input {:type "submit" :id "url" :name "url"}]]
-                  [:button {:type "submit" :value "Next" :onclick (str "window.location=\"./seuraava?huone=" table "\";")} "Next"]
-                  #_[:button {:type "submit" :value "ref" :onclick "document.getElementById('lista').src = document.getElementById('lista').src"} "Refresh Playlist"]]
-                  [:div {:align "center"}
-                  [:iframe {:id "lista"
-                            :width "450"
-                            :height "315"
-                            :src (str "./soittolista?huone=" table)}]])
-            (html [:h
-                   [:script
-                    {:src "https://code.jquery.com/jquery-3.5.1.min.js" :integrity "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" :crossorigin "anonymous"}]
-                   [:script
-                    {:src "https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" :integrity "sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" :crossorigin "anonymous"}]
-                   [:script "$(function() {
-                        $('#lomake').ajaxForm(function() {
-                                  location.reload();
-                              });
-                          });"]]
-                  [:p "Playlist empty"]
-                  [:form {:action "./asd" :method "post" :id "lomake"}
-                    [:input {:type "text" :id "url" :name "url"}]
-                    [:input {:type "hidden" :id "huone" :name "huone" :value table}]
-                    [:input {:type "submit" :id "url" :name "url"}]]))))
 
 
 (defn frontpage [req]
