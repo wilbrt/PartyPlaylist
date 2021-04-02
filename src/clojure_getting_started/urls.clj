ee22f7d713e02fbd375d87d250f7db31ab3bb639
Vilppu Saarinen <vilppusaa@gmail.com>
2021-04-02 11:43:09 +0300

    Added Room name

--- src/clojure_getting_started/urls.clj
+++ src/clojure_getting_started/urls.clj
@@ -182,6 +182,7 @@
         sorsa (get-source-by-id 1 table)]
       (html [:h [:script {:src "https://code.jquery.com/jquery-3.5.1.min.js" :integrity "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" :crossorigin "anonymous"}]
                 [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" :integrity "sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" :crossorigin "anonymous"}]]
+            [:div {:align "center"} [:p (str "ROOM NAME: " table)]]
             [:div {:align "center"} (player table url sorsa)]
             [:div {:align "center"} [:p (get-name-by-id 1 table)]]
             [:div {:align "center"}
