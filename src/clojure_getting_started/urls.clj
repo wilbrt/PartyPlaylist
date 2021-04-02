596eec430d63f0dde426a82997ce6cc1274e7955
Vilppu Saarinen <vilppusaa@gmail.com>
2021-04-02 16:38:36 +0300

    Fixed youtube resize

--- src/clojure_getting_started/urls.clj
+++ src/clojure_getting_started/urls.clj
@@ -153,12 +153,12 @@
       (jdbc/delete! spec (keyword table) ["id = ?" 1])
       (jdbc/execute! spec [(str "update " table " set id = id - 1 where id < ?") 30]))
 
-(defn player [table id source]
+(defn player [table id source b]
   (if (and id source) (cond
                               (= source 0) (html  [:script "$(function() {
                                                 $('#addtolist').ajaxForm(function() {
                                                   document.getElementById('lista').src = document.getElementById('lista').src;});});"]
-                                   [:iframe { :width "560" :height "300" :src (str "https://www.youtube.com/embed/" id "?autoplay=1")
+                                   [:iframe { :width (if b "100%" "560") :height "300" :src (str "https://www.youtube.com/embed/" id "?autoplay=1")
                                               :frameboarder "0" :allow "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"}])
                               (= source 1) (html [:script "$(function() {
                                                 $('#addtolist').ajaxForm(function() {
@@ -179,18 +179,19 @@
 (defn videohaku [req]
   (let [table (get-in req [:params :huone])
         url (get-url-by-id 1 table)
-        sorsa (get-source-by-id 1 table)]
+        sorsa (get-source-by-id 1 table)
+        b (clojure.string/includes? (str (get req :User-Agent)) "Mobile")]
       (html [:h [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
                 [:link {:href "http://fonts.googleapis.com/css?family=Corben:bold" :rel "stylesheet" :type "text/css"}]
                 [:link {:href "http://fonts.googleapis.com/css?family=Nobile" :rel "stylesheet" :type "text/css"}]
-                [:style {:type "text/css"} (str (if (clojure.string/includes? (str (get req :User-Agent)) "Mobile")
-                                                                      "#url { width: 50%; } #source { width: 30%; } #but { width: 20%; } #lista { width: 100%; }")
+                [:style {:type "text/css"} (str (if b "#url { width: 50%; } #source { width: 30%; } #but { width: 20%; } #lista { width: 100%; }")
                               "h1, h2, h3, h4, h5, h6 { font-family: 'Corben', Georgia, Times, serif; font-size: 1.5em; }
-                               p, div { font-family: 'Nobile', Helvetica, Arial, sans-serif; }")]
+                               p, div { font-family: 'Nobile', Helvetica, Arial, sans-serif; }
+                               ")]
                 [:script {:src "https://code.jquery.com/jquery-3.5.1.min.js" :integrity "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" :crossorigin "anonymous"}]
                 [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" :integrity "sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" :crossorigin "anonymous"}]]
             [:div {:align "center"} [:h1 (str "Room Name: " (clojure.string/upper-case (str table)))]]
-            [:div {:align "center"} (player table url sorsa)]
+            [:div {:align "center"} (player table url sorsa b)]
             [:div {:align "center"} [:p (get-name-by-id 1 table)]]
             [:div {:align "center" :id "search"}
                 [:form {:id "addtolist" :action "./asd" :onclick "setTimeout(() => {document.getElementById('lista').src = document.getElementById('lista').src;}, 2000);" :method "post"}
@@ -204,7 +205,7 @@
                 [:button {:type "submit" :value "Next" :onclick (str "window.location=\"./seuraava?huone=" table "\";")} "Next"]
                            #_[:button {:type "submit" :value "ref" :onclick "document.getElementById('lista').src = document.getElementById('lista').src"} "Refresh Playlist"]]
             [:div {:align "center" :style "margin-top: 10px;"}
-                [:iframe {:id "lista" :height "315" :src (str "./soittolista?huone=" table)}]])))
+                [:iframe {:id "lista" :height "315" :allowtransparency "true" :src (str "./soittolista?huone=" table)}]])))
 
 
 (defn frontpage [req]
@@ -229,7 +230,8 @@
 
 (defn soittolista [req]
   (let [table (get-in req [:params :huone])]
-      (html [:table (for [x (range 2 (newsongnumber 1 table))]
+      (html [:h [:style {:type "text/css"} "body { background-color : transparent; }"]]
+            [:table (for [x (range 2 (newsongnumber 1 table))]
                       [:div {:align "center"}
                         [:tr
                         [:td (get-name-by-id x table)]]]
